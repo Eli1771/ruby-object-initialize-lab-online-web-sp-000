116f836e53c85cd61b08baeba3c0c572b6e1d1c1
elijahmm17 <elijahmm17@gmail.com>
2019-05-19 21:17:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,6 @@
 class Dog 
-  def initialize
-    def name=(name)
-      @name = name 
-    end 
-    def name 
-      @name 
-    end 
-    def breed=(breed = "Mutt")
-      @breed = breed 
-    end 
-    def breed 
-      @breed
-    end 
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
   end 
 end 
